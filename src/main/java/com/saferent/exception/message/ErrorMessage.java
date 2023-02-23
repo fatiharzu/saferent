aca9004e8a851d6f3b01d628d2ab7f0d9689a75e
arzu <fatiharzu1@gmail.com>
2023-02-23 19:50:56 +0100

    Initial commit

---
+++ src/main/java/com/saferent/exception/message/ErrorMessage.java
@@ -0,0 +1,10 @@
+package com.saferent.exception.message;
+
+public class ErrorMessage {
+
+    public final static String RESOURCE_NOT_FOUND_EXCEPTION = "Resource with id %s not found";
+    public final static String ROLE_NOT_FOUND_EXCEPTION = "Role : %s not found";
+    public final static String USER_NOT_FOUND_EXCEPTION = "User with email: %s not found";
+    public final static String JWTTOKEN_ERROR_MESSAGE = "JWT Token Validation Error: %s";
+    public final static String EMAIL_ALREADY_EXIST_MESSAGE = "Email: %s already exist";
+}
